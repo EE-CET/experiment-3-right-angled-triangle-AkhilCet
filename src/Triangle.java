16395c53ca3b325b4fb9836693a67f4aa1595fc4
AkhilCet <tve24el005@cet.ac.in>
2026-01-25 19:02:19 +0530

    Update Triangle.java

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,18 @@
 public class Triangle {
     //Add your programs here
+    public static void main(String args[]){
+	
+		int n = 3;
+        int c = 1;
+
+        for(int i=0; i<n; i++){
+            for(int j=0;j<c;j++){
+                System.out.print("*");
+                
+            }
+            System.out.println();
+            c+=2;
+        }
+
+	}
 }
